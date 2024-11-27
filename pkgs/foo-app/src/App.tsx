65f8562147e56d07f506cd559c3b8db90395aa6c
Walker Burgin <wburgin@anduril.com>
2024-11-27 12:03:45 -0800

    Setup vite dev mode

--- pkgs/foo-app/src/App.tsx
+++ pkgs/foo-app/src/App.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { HelloWorld } from "@monorepo/foo-lib";
 
-export const App: React.FC = () => <div><HelloWorld /></div>;
+export const App: React.FC = () => <div><div>App</div><HelloWorld /></div>;
