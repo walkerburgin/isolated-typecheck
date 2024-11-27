633ad52643f0f0b33426ab7dd366351e7165f1ad
Walker Burgin <wburgin@anduril.com>
2024-11-27 12:44:50 -0800

    Add a third package

--- pkgs/foo-app/src/App.tsx
+++ pkgs/foo-app/src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
-import { HelloWorld } from "@monorepo/foo-lib";
+import { Foo } from "@monorepo/foo-lib";
 
-export const App: React.FC = () => <div><div>App</div><HelloWorld /></div>;
+export const App: React.FC = () => (
+    <div>
+        <div>App</div>
+        <Foo />
+    </div>
+);
--- pkgs/foo-lib/src/HelloWorld.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const HelloWorld: React.FC = () => <div>Hello, world</div>;
